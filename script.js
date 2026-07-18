ed390cc0633a366e439d9e0007198b33c0d1588a
15316-cyber <15316@phanthong.ac.th>
2026-07-18 13:03:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
         ]
     });
 });
+const container = document.getElementById("sakura-container");
+
+function createSakura(){
+
+    const sakura = document.createElement("div");
+
+    sakura.classList.add("sakura");
+
+    sakura.innerHTML = "🌸";
+
+    sakura.style.left = Math.random()*100 + "vw";
+
+    sakura.style.fontSize = (18 + Math.random()*18) + "px";
+
+    sakura.style.animationDuration = (5 + Math.random()*5) + "s";
+
+    sakura.style.opacity = Math.random();
+
+    container.appendChild(sakura);
+
+    setTimeout(()=>{
+        sakura.remove();
+    },10000);
+
+}
+
+setInterval(createSakura,250);
